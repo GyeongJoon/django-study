f1271b31e24605ee849068f5a3e0b66ba7f3784a
GyeongJoon <jjangjoon328@naver.com>
2024-01-10 11:22:02 +0900

    update

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from myapp import views
 urlpatterns = [
     path('', views.index),
-        path('create/', views.create),
-        path('read/1/', views.read)
+    path('create/', views.create),
+    path('read/<id>/', views.read)
 ]
