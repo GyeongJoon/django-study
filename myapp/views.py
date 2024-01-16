a218afdbac15539b53b15326bf07197f402d436f
GyeongJoon <jjangjoon328@naver.com>
2024-01-16 15:09:41 +0900

    update

--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,60 @@
 from django.shortcuts import render, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 import random
+
+topics = [
+    {'id': 1, 'title':'routing', 'body':'Routing is..'},
+    {'id': 2, 'title':'view', 'body':'view is..'},
+    {'id': 3, 'title':'model', 'body':'model is..'}
+]
+
+def HTMLTemplate(articleTag):
+    global topics
+    ol = ''
+    for topic in topics:
+        ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
+    return HttpResponse(f'''
+    <html>
+    <body>
+        <h1><a href="/">Django</a></h1>
+        <ul>
+            {ol}
+        </ul>
+        {articleTag}
+        <ul>
+            <li><a href='/create/'>create</a></li>
+            <li><a href='/delete/'>delete</a></li>
+        </ul>
+    </body>
+    </html>
+    ''')
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Random</h1>' + str(random.random()))
-def create(request):
-    return HttpResponse('Create')
+    article ='''
+    <h2>Welcome</h2>
+    Hello, Django
+    '''
+    return HttpResponse(HTMLTemplate(article))
 
 def read(request, id):
-    return HttpResponse('Read!'+ id)
+    global topics
+    article = ''
+    for topic in topics:
+        if topic['id'] == int(id):
+            article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
+    return HttpResponse(HTMLTemplate(article))
+
+@csrf_exempt
+def create(request):
+    article = '''
+        <form action="/create/" method="POST">
+        <p><input type="text" name="title" placeholder="title"></p>
+        <p><textarea name="body" placeholder="body"></textarea></p>
+        <p><input type="submit"></p>
+        </form>
+    '''
+    return HttpResponse(HTMLTemplate(article))
+
+
